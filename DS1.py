dbf8d446188b2e839ed699014eeb3974747535b7
vikaskhullar <vikas.khullar@gmail.com>
2021-01-18 13:13:07 +0530

    update

--- DS1.py
+++ DS1.py
@@ -5,4 +5,83 @@ Created on Sun Jan 17 17:45:46 2021
 @author: vikas
 """
 
-     kdjd
+'    '
+"    "
+
+print ('Hi Welcome to Python')
+print ('Welcome to Python')
+print ('Welcome to Python')
+
+#Python help
+
+print?
+
+help(print)
+
+help()
+
+print?
+
+
+'''
+\n - newline
+\t - tab
+'''
+
+#\n - newline
+
+
+print("Hi Welcome", end='\n')
+print ('Welcome to Python' )
+
+
+print ('Hi', 'Welcome', 'To','Python', sep='\n')
+
+print ('Hi', 'Welcome', 'To','Python', sep='-', end='\n')
+print ('Hi', 'Welcome', 'To','Python', sep='%')
+
+
+
+import pandas
+
+pandas.__version__
+
+import numpy
+
+import matplotlib.pyplot
+
+
+
+!pip install pandas
+
+!pip list
+
+
+!python -m pip install --upgrade pip
+
+!pip --version
+
+
+!conda update conda
+
+
+!pip install --upgrade pandas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DS2.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 19:51:40 2021
+
+@author: vikas
+"""
+
+'''
+Normal Programming
+
+int a;
+a = 10;
+
+'''
+
+#Static Input
+
+a = 10
+b= 10.5
+c = True
+country = 'India'
+
+
+print (a)
+
+print (country)
+
+country1 = input()
+
+print (country1)
+
+
+
+country2 = input("Enter Country Name->  ")
+
+country2
+
+
+print ("I live in", country)
+
+print ("I live in {0}".format(country))
+
+print ("I live in {0}, want to move {1}".format(country, country1))
+
+print ("I live in {1}, want to move {0}".format(country, country1))
+
+
+a = 10
+b = 10.5
+c = True
+d = '10.5'
+e = '10'
+
+
+# Type Casting or Type Conversion
+
+type(a)
+af = float(a)
+type(af)
+print (af)
+
+ast = str(a)
+type(ast)
+print(ast)
+
+type(b)
+bi = int(b)
+type(bi)
+print (bi)
+
+bs = str(b)
+type(bs)
+print (bs)
+
+ei = int(e)
+type(ei)
+print (ei)
+
+df = float(d)
+type(df)
+print (df)
+
+
+asc =chr(97)
+asc
+
+asc =chr(100)
+asc
+
+asc =chr(33)
+asc
+
+
+ascr = ord('a')
+ascr
+
+ascr = ord('!')
+ascr
+
+
+
+x = 2
+
+print (x + 1)
+
+x = x + 1
+
+print (x)
+
+y = x + 1
+
+x = 5
+print (x)
+print (x + 2)
+print (x - 2)
+print (x * 2)
+print (x / 2)
+print (x % 2) # Moduls
+print (x ** 3)
+
+'''
+x=9
+print (x ** 1/3)
+'''
+
+
+x = x + 1
+x
+
+x += 1
+x
+
+x **= 2
+x
+
+
+print (x, x+2, x-2, x*2 )
+
+
+
+t = True
+f = False
+
+print (t)
+print (f)
+
+
+AND
+0 0 0
+0 1 0
+1 0 0
+1 1 1
+
+OR
+0 0 0
+0 1 1
+1 0 1
+1 1 1
+
+NOT
+0 1
+1 0
+
+
+print (t and f)
+print (t or f)
+
+
+t = True
+f = False
+print (t and f)
+print (t or f)
+
+print (not (f))
+
+
+Fname = 'vikas'
+Lname = 'khullar'
+
+Name = Fname + Lname
+Name
+
+Name = Fname + ' ' + Lname
+Name
+
+print (len(Name))
+
+Name
+
+Name = Name.capitalize()
+Name
+
+Name = Name.upper()
+Name
+
+Name = Name.lower()
+Name
+
+Name= 'Vikas'
+
+Name = Name.rjust(10)
+
+print(Name)
+
+Name= 'Vikas'
+
+Name = Name.center(10)
+
+print(Name)
+
+
+Name= 'Vikas'
+
+Name = Name.ljust(10)
+
+print(Name)
+
+s = '@Hello Programming'
+
+s1 = s.replace('@', '')
+s1
+
+
+s = '       Hello      Programmers       '
+
+s
+
+s = s.strip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
